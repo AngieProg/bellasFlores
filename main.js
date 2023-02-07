5feb8d3fdb1f87bb1cdca9c51e89200f4e054b31
AngieProg <angie_db@hotmail.com>
2023-02-07 14:32:17 -0600

    cambios interlineado

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded',() => {
     const elementosCarousel = document.querySelectorAll( '.carousel');
     M.Carousel.init(elementosCarousel, {
         duration: 150,
-        dist: -80,
+        // dist: -80,
         shift: 5,
         padding: 5,
         numVisible: 1,
